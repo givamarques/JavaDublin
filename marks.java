9ec9e25c7c32d30a7dc1ce92e4737a148bbb69b1
givamarques <givamarques@users.noreply.github.com>
2021-03-15 12:48:46 -0300

    01

---
+++ marks.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Q3{
+    public static void main(String[] args) {
+        Scanner mark= new Scanner(System.in);
+
+        System.out.println("Qual a nota, bença? ");
+        double m1 = mark.nextDouble();
+        double mark;
+
+        if (m1<0 || m1>100){
+            System.out.println("É a nota, não é o saldo não, Bença! ");
+        } else if{
+            System.out.println((m1/100)*20+"Qual a nota, bença? ");
+        }
+
+    }
+}
