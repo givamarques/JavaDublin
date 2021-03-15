6d97fdd97da12992e0068228358a05aa376f89c1
givaldo santos <givaldosantos@live.com>
2021-03-15 14:01:46 -0300

    so faltam as letras

--- Q3.java
+++ Q3.java
@@ -4,13 +4,13 @@ public class Q3{
     public static void main(String[] args) {
         Scanner mar= new Scanner(System.in);
 
-        System.out.println("Qual a nota, bença? ");
+        System.out.println("Qual tua nota, bença? ");
         double m1 = mar.nextDouble();
         double mark;
 
-        if (m1>=0 && m1<=100){
-            System.out.println("Tua nota é: "+(m1/100)*20+"(Fraca, hein Bença?)");
-        } else{
+        if (m1>=0 && m1<=100) {
+            System.out.println("Tua nota é: "+(m1/100)*20+" (Fraca, hein Bença?)");
+        } else {
             System.out.println("É a nota não é o saldo não, bença!");
         }
     }
